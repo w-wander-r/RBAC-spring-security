ba0d29a6d73044c62a424e15713555022b1d9cba
w-wander-r <wanderexe68@gmail.com>
2025-02-20 21:33:06 +0200

    user entity added

---
+++ src/main/java/com/wander/roles/User.java
@@ -0,0 +1,33 @@
+package com.wander.roles;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles", 
+               joinColumns = @JoinColumn(name = "user_id"), 
+               inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
